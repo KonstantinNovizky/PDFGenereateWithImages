45ae568b24df04b1d187f0b84213619d20ffbf70
KonstantinNovizky <konstantinnovizky@gmail.com>
2021-05-07 09:24:38 +0700

    init

---
+++ public/js/response.js
@@ -0,0 +1,100 @@
+var responseData = '{\n' +
+    '    "success":true,\n' +
+    '    "result":{\n' +
+    '        "total":1,\n' +
+    '        "listings":[{\n' +
+    '            "id":"SR20245660",\n' +
+    '            "market":"crmls",\n' +
+    '            "geoType":"listing",\n' +
+    '            "systemId":"495820907",\n' +
+    '            "address":{\n' +
+    '                "deliveryLine":"2006 Castle Heights Avenue",\n' +
+    '                "city":"Los Angeles",\n' +
+    '                "state":"CA",\n' +
+    '                "zip":"90034",\n' +
+    '                "street":"2006 Castle Heights Avenue"\n' +
+    '            },\n' +
+    '            "area":"C09 - Beverlywood Vicinity",\n' +
+    '            "baths":{\n' +
+    '                "total":3,\n' +
+    '                "full":3,\n' +
+    '                "half":0\n' +
+    '            },\n' +
+    '            "beds":3,\n' +
+    '            "coordinates":{\n' +
+    '                "latitude":34.046191,\n' +
+    '                "longitude":-118.398083\n' +
+    '            },\n' +
+    '            "county":"Los Angeles",\n' +
+    '            "daysOnMarket":0,\n' +
+    '            "description":"A well loved home is waiting for a new owner. Rare opportunity to own an almost 14,000 sq ft flat lot in the desirable community of Beverlywood. The potential is grand. There is currently a 2368sf home and a cabana structure by the pool on the property.",\n' +
+    '            "floors":"1",\n' +
+    '            "imageCount":11,\n' +
+    '            "lastUpdated":1619656308,\n' +
+    '            "listPrice":3295000,\n' +
+    '            "listingAgent":{\n' +
+    '                "id":"F210004110",\n' +
+    '                "name":"Cindee Zabner",\n' +
+    '                "email":"Cindee@TheZabnerGroup.com"\n' +
+    '            },\n' +
+    '            "listingDate":1606262400,\n' +
+    '            "listingOffice":{\n' +
+    '                "id":"F0988001",\n' +
+    '                "name":"Keller Williams Realty World Class",\n' +
+    '                "email":"KLRW207@KW.COM",\n' +
+    '                "phone":"805-777-7777"\n' +
+    '            },\n' +
+    '            "listingType":"Residential",\n' +
+    '            "lotSize":{\n' +
+    '                "sqft":13848,\n' +
+    '                "acres":0.32\n' +
+    '            },\n' +
+    '            "modifiedDate":1610144192,\n' +
+    '            "newConstruction":false,\n' +
+    '            "offMarketDate":1609891200,\n' +
+    '            "propertyType":"Single Family Residence",\n' +
+    '            "salePrice":3200000,\n' +
+    '            "schools":{\n' +
+    '                "district":"Los Angeles Unified"\n' +
+    '                },\n' +
+    '            "sellingAgent":{\n' +
+    '                "id":"CLW-X10720",\n' +
+    '                "name":"Jacob Hausman",\n' +
+    '                "email":"jacob@therfcgroup.com",\n' +
+    '                "phone":"310-275-5156"\n' +
+    '            },\n' +
+    '            "sellingOffice":{\n' +
+    '                "id":"CLW-X70221",\n' +
+    '                "name":"The RFC Group"\n' +
+    '            },\n' +
+    '            "size":2368,\n' +
+    '            "status":"Closed",\n' +
+    '            "stdAddress":{\n' +
+    '                "deliveryLine":"2006 Castle Heights Ave",\n' +
+    '                "city":"Los Angeles",\n' +
+    '                "state":"CA",\n' +
+    '                "zip":"90034",\n' +
+    '                "street":"2006 Castle Heights Ave"\n' +
+    '            },\n' +
+    '            "style":"Ranch",\n' +
+    '            "yearBuilt":1952,\n' +
+    '            "images":[\n' +
+    '                "https://listing-images.homejunction.com/crmls/495820907/photo_1.jpg",\n' +
+    '                "https://listing-images.homejunction.com/crmls/495820907/photo_2.jpg",\n' +
+    '                "https://listing-images.homejunction.com/crmls/495820907/photo_3.jpg",\n' +
+    '                "https://listing-images.homejunction.com/crmls/495820907/photo_4.jpg",\n' +
+    '                "https://listing-images.homejunction.com/crmls/495820907/photo_5.jpg",\n' +
+    '                "https://listing-images.homejunction.com/crmls/495820907/photo_6.jpg",\n' +
+    '                "https://listing-images.homejunction.com/crmls/495820907/photo_7.jpg",\n' +
+    '                "https://listing-images.homejunction.com/crmls/495820907/photo_8.jpg",\n' +
+    '                "https://listing-images.homejunction.com/crmls/495820907/photo_9.jpg",\n' +
+    '                "https://listing-images.homejunction.com/crmls/495820907/photo_10.jpg",\n' +
+    '                "https://listing-images.homejunction.com/crmls/495820907/photo_11.jpg"]\n' +
+    '        }],\n' +
+    '        "paging":{\n' +
+    '           "count":1,\n' +
+    '           "number":1,\n' +
+    '           "size":25\n' +
+    '        }\n' +
+    '    }\n' +
+    '}';
